f9a2061feaf951348e0b912cd3ba4e5cdc07f08d
BradfordBach <bbach@lumere.com>
2019-05-19 23:23:06 -0500

    Initial Commit
    This is the complete working code to get locations display and save it to a log and to put it in your clipboard for easy pasting

---
+++ NMS_Locator.py
@@ -0,0 +1,82 @@
+import json
+import os
+import datetime
+import threading
+import pyperclip
+
+def get_current_location():
+    my_save = open(save, "r")
+
+    my_save_string = my_save.read()[:-1]
+    complete_save = json.loads(my_save_string)
+
+    json_player_data = complete_save['6f=']["yhJ"]["oZw"]
+
+    Player_State = {'x':json_player_data["dZj"], 'y':json_player_data['IyE'], 'z':json_player_data["uXE"], 'ssi':json_player_data['vby']}
+    galactic_address = format_galaxtic_coord(Player_State['x'], Player_State['y'], Player_State['z'], Player_State['ssi'])
+
+    if not check_if_address_exists(galactic_address):
+        time_logged = datetime.datetime.now()
+        enter_address_into_log(galactic_address, time_logged)
+        print(time_logged.strftime("%B %d %I:%M:%S %p -> "), end='')
+        print(galactic_address)
+        pyperclip.copy(galactic_address)
+
+def get_file_mod_time():
+    return os.path.getmtime(save)
+
+def get_latest_save_file():
+    all_save_files = []
+    for(dirpath, dirnames, filenames) in os.walk(os.getenv('APPDATA') + os.sep + "HelloGames" + os.sep + "NMS"):
+        for file in filenames:
+            if file[-3:] == ".hg" and file[:3] != "mf_":
+                all_save_files.append(os.path.join(dirpath, file))
+    latest_save = max(all_save_files, key=os.path.getmtime)
+    return latest_save
+
+def check_if_address_exists(galactic_address):
+    if galactic_address in location_log:
+        return True
+
+def enter_address_into_log(galactic_address, dt):
+    with open(log_loc, "a") as log:
+        log.write(dt.strftime("%B %d %I:%M:%S %p") + ',' + galactic_address + '\n')
+
+def format_galaxtic_coord(x, y, z, ssi):
+    fmt = "{0:0{1}X}"
+    parts = [str(fmt.format(x + 2047, 4)), str(fmt.format(y + 127, 4)), str(fmt.format(z + 2047, 4)), str(fmt.format(ssi,4))]
+    return(':'.join(parts))
+
+def load_log():
+    log_dir = os.getenv('LOCALAPPDATA') + os.sep + 'Programs' + os.sep + "NMS Locator"
+    try:
+        os.makedirs(log_dir)
+    except FileExistsError:
+        pass
+    try:
+        open(log_dir + os.sep + "location_log.log", 'r')
+    except IOError:
+        open(log_dir + os.sep + "location_log.log", 'w')
+
+    location_log = []
+    with open(log_dir + os.sep + "location_log.log") as log:
+        line = log.readline().strip()
+        while line:
+            location_log.append(line.split(',')[1])
+            line = log.readline().strip()
+
+    return location_log, log_dir + os.sep + "location_log.log"
+
+def run_location_gatherer():
+    threading.Timer(15.0, run_location_gatherer).start()
+    if get_file_mod_time() not in mod_times:
+        mod_times.append(get_file_mod_time())
+        get_current_location()
+    if len(mod_times) >= 200:
+        mod_times.clear()
+
+location_log, log_loc = load_log()
+save = get_latest_save_file()
+mod_times = []
+print("Waiting for new locations every 15 seconds.\nNew locations will be copied to clipboard and displayed...")
+run_location_gatherer()
